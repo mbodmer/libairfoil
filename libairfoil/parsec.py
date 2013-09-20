82766686757fb7b3a23e5cf9e904153096a82d62
mbodmer <marc.bodmer@email.ch>
2013-09-21 01:07:05 +0200

    Fileheader license added

--- libairfoil/parsec.py
+++ libairfoil/parsec.py
@@ -1,4 +1,10 @@
 '''
+#          Copyright Marc Bodmer 2013.
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+
 Python library for parametric representation of airfoils according to PARSEC (PARametric SECtion).
 
 References:
