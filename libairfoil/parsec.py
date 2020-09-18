77bde81fe62e0cdb754027260765046feb97d698
mbodmer <marc.bodmer@email.ch>
2020-09-19 00:47:02 +0200

    move parsec main to examples
    Signed-off-by: mbodmer <marc.bodmer@email.ch>

--- libairfoil/parsec.py
+++ libairfoil/parsec.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 '''
 #          Copyright Marc Bodmer 2013.
 # Distributed under the Boost Software License, Version 1.0.
@@ -99,45 +97,3 @@ class Airfoil(object):
         a = self._coeff.a_lo()
         print(a)
         return a[0]*X**0.5 + a[1]*X**1.5 + a[2]*X**2.5 + a[3]*X**3.5 + a[4]*X**4.5 + a[5]*X**5.5
-
-
-if __name__=='__main__':
-    import matplotlib.pyplot
-    
-    params = Parameters()
-#     params.r_le       = 0.02
-#     params.X_up       = 0.35
-#     params.Z_up       = 0.08
-#     params.Z_XX_up    = -0.65
-#     params.X_lo       = 0.25
-#     params.Z_lo       = -0.08
-#     params.Z_XX_lo    = 0.6
-#     params.Z_te       = 0.0
-#     params.dZ_te      = 0.04
-#     params.alpha_te   = math.radians(-5.0)
-#     params.beta_te    = math.radians(8.0)
-
-#R_LE;  x_PRE;y_PRE;d2y/dx2_PRE;th_PRE;  x_SUC;y_SUC;d2y/dx2_SUC;th_SUC
-#0.01;  0.450;-0.006;-0.2;0.05;          0.350;0.055;-0.350;-6
-
-#Parsec-11 [0,01: 0,4: 0,075: -0,1: 0,075: 0,075: -0,1: 0: -0: 0: 20]
-
-    params.r_le       = 0.01
-    params.X_up       = 0.4
-    params.Z_up       = 0.075
-    params.Z_XX_up    = -0.1
-    params.X_lo       = 0.4
-    params.Z_lo       = -0.075
-    params.Z_XX_lo    = 0.1
-    params.Z_te       = 0.0
-    params.dZ_te      = 0.0
-    params.alpha_te   = math.radians(0.0)
-    params.beta_te    = math.radians(20.0)
-    
-    airfoil = Airfoil(params)
-    
-    matplotlib.pyplot.figure()
-    x = numpy.linspace(0.0, 1.0, 200)
-    matplotlib.pyplot.plot(x, airfoil.Z_up(x), 'r--', x, airfoil.Z_lo(x), 'b--')
-    matplotlib.pyplot.grid(True)
-    matplotlib.pyplot.show()
