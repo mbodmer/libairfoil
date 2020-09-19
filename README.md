79b6247ebe5e4a9e6613aee8bfdbe36d4fb59e82
mbodmer <marc.bodmer@email.ch>
2020-09-19 20:24:42 +0200

    added another example parsec airfoil
    Signed-off-by: mbodmer <marc.bodmer@email.ch>

--- README.md
+++ README.md
@@ -24,10 +24,15 @@ poetry run pytest -s
 poetry run parsec
 ```
 
+Parsec-11 [0.0083:0.423:0.0587:-0.347:0.358:-0.032:0.417:0:0:10.03:5.64]
+
+![Parsec Demo Profile](examples/images/parsec3.png)
+
 Parsec-11 [0.01:0.4:0.075:-0.1:0.4:-0.075:0.1:0:0:0:20]
 
-![Parsec Demo Profile](parsec.png)
+![Parsec Demo Profile](examples/images/parsec.png)
 
 Parsec-11 [0.06:0.3:0.118:-0.9:0.3:-0.118:0.9:0:0:0:20]
 
-![Parsec Demo Profile](parsec2.png)
+![Parsec Demo Profile](examples/images/parsec2.png)
+
--- examples/parsec.py
+++ examples/parsec.py
@@ -9,8 +9,9 @@ import libairfoil.parsec
 
 def main():
        
-    jfoil_string = 'Parsec-11 [0.01:0.4:0.075:-0.1:0.4:-0.075:0.1:0:0:0:20]'
+    # jfoil_string = 'Parsec-11 [0.01:0.4:0.075:-0.1:0.4:-0.075:0.1:0:0:0:20]'
     # jfoil_string = 'Parsec-11 [0.06:0.3:0.118:-0.9:0.3:-0.118:0.9:0:0:0:20]'
+    jfoil_string = 'Parsec-11 [0.0083:0.423:0.0587:-0.347:0.358:-0.032:0.417:0:0:10.03:5.64]'
 
     params = libairfoil.parsec.Parameters()
     params.load_from_javafoil_parsec11(jfoil_string)
